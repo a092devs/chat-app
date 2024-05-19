1ceca645907154e8ccb5376a5cb128edb592864c
a092devs <arsalanakhtar0@gmail.com>
2024-05-19 11:18:29 +0530

    removed phone icon

--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -1,23 +1,22 @@
-import React from "react";
-import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import {ChatBubbleLeftRightIcon} from '@heroicons/react/24/solid';
+import React from 'react';
 
-const CustomerHeader = ({ chat }) => {
-  return (
-    <div className="chat-header">
-      <div className="flexbetween">
-        <ChatBubbleLeftRightIcon className="icon-chat" />
-        <h3 className="header-text">{chat.title}</h3>
-      </div>
-      <div className="flexbetween">
-        <PhoneIcon className="icon-phone" />
-        {chat.description !== "⬅️ ⬅️ ⬅️" ? (
-          <p className="header-text">{chat.description}</p>
-        ) : (
-          <p className="header-text">no chat selected</p>
-        )}
-      </div>
-    </div>
-  );
+const CustomerHeader = ({chat}) => {
+    return (
+        <div className="chat-header">
+            <div className="flexbetween">
+                <ChatBubbleLeftRightIcon className="icon-chat" />
+                <h3 className="header-text">{chat.title}</h3>
+            </div>
+            <div className="flexbetween">
+                {chat.description !== '⬅️ ⬅️ ⬅️' ? (
+                    <p className="header-text">{chat.description}</p>
+                ) : (
+                    <p className="header-text">no chat selected</p>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default CustomerHeader;
